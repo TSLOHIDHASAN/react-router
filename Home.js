e6d2bf0260e7267810108a033a59d4732b5812ba
TSLOHIDHASAN <lohidhasan2020@gmail.com>
2020-08-11 18:49:55 +0530

    Initial commit

---
+++ Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Home(){
+return(
+  <div>
+  <h1>home page render</h1>
+  </div>
+);
+}
+export default Home;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1vyugz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1vyugz)
